94e3cfdb57cf44274fac4b2647ed372cde0ee8a5
test <>
2018-06-29 12:25:53 +0900

    つか

--- video.php
+++ video.php
@@ -2,7 +2,14 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width,user-scalable=no,maximum-scale=1" />
+    <link rel="stylesheet" media="all" type="text/css" href="style.css" />
+    <!-- ※デフォルトのスタイル（style.css） -->
+    <link rel="stylesheet" media="all" type="text/css" href="tablet.css" />
+    <!-- ※タブレット用のスタイル（tablet.css） -->
+    <link rel="stylesheet" media="all" type="text/css" href="smart.css" />
+    <!-- ※スマートフォン用のスタイル（smart.css） -->
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>映像（保護者）</title>
     <link rel="stylesheet" href="css/video.css">
